0347692e0e3038ce4a53eda654d944c30723d984
sega <senior.kapitoshka@gmail.com>
2025-07-01 23:25:45 +0500

    second

--- task_1/custom/template.php
+++ task_1/custom/template.php
@@ -1,40 +1,25 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<?$this->setFrameMode(true);?>
-<?$APPLICATION->SetAdditionalCSS($templateFolder."/style.css");?>
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$APPLICATION->SetAdditionalCSS($templateFolder . "/style.css");
+?>
 
-<div class="news-list-wrapper">
-    <?if($arParams["DISPLAY_TOP_PAGER"]):?>
-        <div class="news-pagination top"><?=$arResult["NAV_STRING"]?></div>
-    <?endif;?>
-
-    <div class="news-list">
-        <?foreach($arResult["ITEMS"] as $arItem):?>
-            <?
-            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            <div class="news-card" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-
-                <div class="news-card__content">
-                    <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-                        <div class="news-card__date"><?=$arItem["DISPLAY_ACTIVE_FROM"]?></div>
-                    <?endif;?>
-
-                    <div class="news-card__title">
-                        <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-                            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
-                        <?endif;?>
-                    </div>
-
-                    <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-                        <div class="news-card__preview"><?=$arItem["PREVIEW_TEXT"]?></div>
-                    <?endif;?>
-                </div>
-            </div>
-        <?endforeach;?>
-    </div>
-
-    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-        <div class="news-pagination bottom"><?=$arResult["NAV_STRING"]?></div>
-    <?endif;?>
+<div id="barba-wrapper">
+  <div class="article-list">
+    <?php foreach ($arResult["ITEMS"] as $item): ?>
+      <a class="article-item article-list__item" href="<?= $item["DETAIL_PAGE_URL"] ?>" data-anim="anim-3">
+        <div class="article-item__background">
+          <?php if (is_array($item["PREVIEW_PICTURE"])): ?>
+            <img
+              src="<?= $item["PREVIEW_PICTURE"]["SRC"] ?>"
+              alt="<?= htmlspecialchars($item["NAME"]) ?>"
+            />
+          <?php endif; ?>
+        </div>
+        <div class="article-item__wrapper">
+          <div class="article-item__title"><?= htmlspecialchars($item["NAME"]) ?></div>
+          <div class="article-item__content"><?= $item["PREVIEW_TEXT"] ?></div>
+        </div>
+      </a>
+    <?php endforeach; ?>
+  </div>
 </div>
