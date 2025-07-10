fa60b5ae09eb163765c8c4076ee8e514fdab861e
sega <senior.kapitoshka@gmail.com>
2025-07-11 00:48:05 +0500

    task_2

--- task_2/webform/template.php
+++ task_2/webform/template.php
@@ -1,5 +1,10 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 
+<pre><?php print_r($_POST); ?></pre>
+<?php if ($arResult["isFormErrors"] == "Y"): ?>
+    <pre><?php print_r($arResult["FORM_ERRORS"]); ?></pre>
+<?php endif; ?>
+
 
 <div class="contact-form">
     <div class="contact-form__head">
@@ -8,76 +13,97 @@
             <div class="contact-form__head-text"><?=$arResult['FORM_DESCRIPTION']?></div>
         <?php endif; ?>
     </div>
+
+    <?php if ($arResult["isFormErrors"] == "Y"): ?>
+        <div class="form-error"><?=$arResult["FORM_ERRORS_TEXT"]?></div>
+    <?php endif; ?>
+
+    <?php if ($arResult["isFormNote"] == "Y"): ?>
+        <div class="form-success"><?=$arResult["FORM_NOTE"]?></div>
+    <?php else: ?>
+
     <form
         class="contact-form__form"
         method="POST"
+        action="<?=$arResult["FORM_ACTION"]?>"
     >
-    <div class="contact-form__form-inputs">
-        <?php 
-        if (isset($arResult['arAnswers']['name']) && is_array($arResult['arAnswers']['name'])):
-            $input_name = "form_text_" . $arResult['arAnswers']['name'][0]['ID'];
-        ?>
-                <div class="input contact-form__input"><label class="input__label" for="medicine_name">
-                    <div class="input__label-text">Ваше имя*</div>
-                    <input class="input__input" type="text" id="medicine_name" name="<?=$input_name?>" value=""
-                    required="">
-                    <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
-                </label></div>
-        <?php endif; ?>
+        <?=bitrix_sessid_post()?>
+        <input type="hidden" name="WEB_FORM_ID" value="<?=$arResult["arForm"]["ID"]?>">
+        <input type="hidden" name="web_form_submit" value="Y">
 
-        <?php 
-        if (isset($arResult['arAnswers']['company']) && is_array($arResult['arAnswers']['company'])):
-            $input_name = "form_text_" . $arResult['arAnswers']['company'][0]['ID'];
-        ?>
-                <div class="input contact-form__input"><label class="input__label" for="medicine_company">
-                    <div class="input__label-text">Компания/Должность*</div>
-                    <input class="input__input" type="text" id="medicine_company" name="<?=$input_name?>" value=""
-                    required="">
-                    <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
-                </label></div>
-        <?php endif; ?>
+        <div class="contact-form__form-inputs">
+            <?php 
+            if (isset($arResult['arAnswers']['name'])):
+                $input_name = "form_text_" . $arResult['arAnswers']['name'][0]['ID'];
+            ?>
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_name">
+                        <div class="input__label-text">Ваше имя*</div>
+                        <input class="input__input" type="text" id="medicine_name" name="<?=$input_name?>" required>
+                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                    </label>
+                </div>
+            <?php endif; ?>
 
-        <?php 
-        if (isset($arResult['arAnswers']['email']) && is_array($arResult['arAnswers']['email'])):
-            $input_name = "form_email_" . $arResult['arAnswers']['email'][0]['ID'];
-        ?>
-                <div class="input contact-form__input"><label class="input__label" for="medicine_email">
-                    <div class="input__label-text">Email*</div>
-                    <input class="input__input" type="email" id="medicine_email" name="<?=$input_name?>" value=""
-                    required="">
-                    <div class="input__notification">Неверный формат почты</div>
-                </label></div>
-        <?php endif; ?>
+            <?php 
+            if (isset($arResult['arAnswers']['company'])):
+                $input_name = "form_text_" . $arResult['arAnswers']['company'][0]['ID'];
+            ?>
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_company">
+                        <div class="input__label-text">Компания/Должность*</div>
+                        <input class="input__input" type="text" id="medicine_company" name="<?=$input_name?>" required>
+                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                    </label>
+                </div>
+            <?php endif; ?>
 
-        <?php 
-        if (isset($arResult['arAnswers']['phone']) && is_array($arResult['arAnswers']['phone'])):
-            $input_name = "form_text_" . $ans_id = $arResult['arAnswers']['phone'][0]['ID'];
-        ?>
-                <div class="input contact-form__input"><label class="input__label" for="medicine_phone">
-                    <div class="input__label-text">Номер телефона*</div>
-                    <input class="input__input" type="tel" id="medicine_phone" data-inputmask="'mask': '+79999999999',
-                    'clearIncomplete': 'true'" maxlength="12" x-autocompletetype="phone-full" name="<?=$input_name?>"
-                    value="" required="">
-                </label></div>
-        <?php endif; ?>
-    </div>
+            <?php 
+            if (isset($arResult['arAnswers']['email'])):
+                $input_name = "form_email_" . $arResult['arAnswers']['email'][0]['ID'];
+            ?>
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_email">
+                        <div class="input__label-text">Email*</div>
+                        <input class="input__input" type="email" id="medicine_email" name="<?=$input_name?>" required>
+                        <div class="input__notification">Неверный формат почты</div>
+                    </label>
+                </div>
+            <?php endif; ?>
+
+            <?php 
+            if (isset($arResult['arAnswers']['phone'])):
+                $input_name = "form_text_" . $arResult['arAnswers']['phone'][0]['ID'];
+            ?>
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_phone">
+                        <div class="input__label-text">Номер телефона*</div>
+                        <input class="input__input" type="tel" id="medicine_phone"
+                            data-inputmask="'mask': '+79999999999','clearIncomplete': 'true'"
+                            maxlength="12" name="<?=$input_name?>" required>
+                    </label>
+                </div>
+            <?php endif; ?>
+        </div>
 
         <?php 
         if (isset($arResult['arAnswers']['message']) && is_array($arResult['arAnswers']['message'])):
-            $input_name = "form_textarea_" . $arResult['arAnswers']['message'][0]['ID'];
+            $input_name = "form_text_" . $arResult['arAnswers']['message'][0]['ID'];
         ?>
             <div class="contact-form__form-message">
-                <div class="input"><label class="input__label" for="medicine_message">
-                    <div class="input__label-text">Сообщение</div>
-                    <textarea class="input__input" type="text" id="medicine_message" name="<?=$input_name?>" value=""></textarea>
-                    <div class="input__notification"></div>
-                </label></div>
+                <div class="input">
+                    <label class="input__label" for="medicine_message">
+                        <div class="input__label-text">Сообщение</div>
+                        <textarea class="input__input" id="medicine_message" name="<?=$input_name?>" required><?=htmlspecialchars($_POST[$input_name] ?? '')?></textarea>
+                    </label>
+                </div>
             </div>
         <?php endif; ?>
 
+
         <div class="contact-form__bottom">
             <div class="contact-form__bottom-policy">
-                Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp; принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.
+                Нажимая &laquo;Отправить&raquo;, Вы подтверждаете, что ознакомлены и принимаете условия &laquo;Согласия на обработку персональных данных&raquo;.
             </div>
             <button type="submit" name="web_form_send" class="form-button contact-form__bottom-button" 
             data-success="Отправлено"
@@ -85,5 +111,6 @@
                 <div class="form-button__title">Оставить заявку</div>
             </button>
         </div>
+    </form>
+    <?php endif; ?>
 </div>
-
